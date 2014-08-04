e816d9436ff85e9591522e5bf9b4b378a7092dc2
Vincent <v.zh@msn.com>
2014-08-05 00:41:30 +0800

    beautify backup scripts

--- backup.sh
+++ backup.sh
@@ -1,23 +1,2 @@
 #! /bin/bash
-
-# bash related
-rsync -avz /home/vincent/.bashrc .bashrc
-rsync -avz /home/vincent/.bash_ps1 .bash_ps1
-rsync -avz /home/vincent/.bash_aliases .bash_aliases
-rsync -avz /home/vincent/.bash_constants .bash_constants
-rsync -avz /home/vincent/.bash_functions .bash_functions
-rsync -avz /home/vincent/.bash_profile .bash_profile
-
-# terminator configuration
-rsync -avz /home/vincent/.config/terminator/config .config/terminator/config
-
-# vim
-rsync -avz /home/vincent/.vimrc .vimrc
-rsync -avz /home/vincent/.vim/ .vim/
-rsync -avz /home/vincent/.vim-fuf-data/ .vim-fuf-data/
-
-# geit
-rsync -avz /home/vincent/.local/share/gedit/ .local/share/gedit/
-
-# misc
-rsync -avz /home/vincent/.dircolors .dircolors
+rsync -avz --files-from=backup_list ~ .
