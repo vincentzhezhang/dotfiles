5fdc0a26f7130288c62fed2b7b44fe808cc51490
Vincent Zhang <vincent.zhang@akunacapital.com>
2018-03-13 23:04:34 +0800

    Some color fixes

--- scripts/256_colors.sh
+++ scripts/256_colors.sh
@@ -7,14 +7,15 @@
 # http://sam.zoy.org/wtfpl/COPYING for more details.
 
 for fgbg in 38 48 ; do #Foreground/Background
-    for color in {0..256} ; do #Colors
-        #Display the color
-        echo -en "\e[${fgbg};5;${color}m ${color}\t\e[0m"
-        #Display 10 colors per lines
-        if [ $((($color + 1) % 8)) == 0 ] ; then
-            echo #New line
-        fi
+  for color in {0..16} {232..256}; do #Colors
+    for attr in 0 1 2 3 4 7; do # Attr: normal bold dim italic underline inverted
+      #Display the color
+      color_code="${attr};${fgbg};5;${color}"
+      echo -en "\e[${color_code}m ${color_code}\t\e[0m"
+      #Display 10 colors per lines
     done
     echo #New line
+  done
+  echo #New line
 done
 exit 0
