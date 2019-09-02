2b0fcebad47fc1b04ff8e6c556b197de61cbda7b
Vincent Zhang <v.zh@msn.com>
2019-09-02 17:52:13 +1000

    refactor: make use of the enhanced utils

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -2,25 +2,27 @@
 
 __.log()
 {
-  >&2 echo "$*"
+  >&2 echo ">>> $@"
 }
 
 __.log_success()
 {
-  >&2 echo -e "\\e[32m$*\\e[0m"
+  >&2 echo -e "\\e[32m>>> [$(basename $0):${BASH_LINENO[1]}] $@\\e[0m"
 }
 
 __.log_info()
 {
-  >&2 echo -e "\\e[36m$*\\e[0m"
+  >&2 echo -e "\\e[36m>>> [$(basename $0):${BASH_LINENO[1]}] $@\\e[0m"
 }
 
 __.log_warning()
 {
-  >&2 echo -e "\\e[33m$*\\e[0m"
+  >&2 echo -e "\\e[33m>>> [$(basename $0):${BASH_LINENO[1]}] $@\\e[0m"
 }
 
 __.log_error()
 {
-  >&2 echo -e "\\e[1;31m$*\\e[0m"
+  >&2 echo -e "\\e[1;31m>>> [$(basename $0):${BASH_LINENO[1]}] $@\\e[0m"
 }
+
+# vim: set autoindent expandtab number textwidth=119 tabstop=2 shiftwidth=2 :
